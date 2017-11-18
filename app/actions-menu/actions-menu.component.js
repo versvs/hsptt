39cd5a41648d08bb2a4e0f944add15e196f35902
Unknown <versvs@gmail.com>
2017-11-18 13:05:25 +0100

    Export feature and basic reset feature

--- app/actions-menu/actions-menu.component.js
+++ app/actions-menu/actions-menu.component.js
@@ -6,7 +6,37 @@ angular.
   component('actionsMenu', {
     templateUrl: 'app/actions-menu/actions-menu.template.html',
     styleUrls: ['app/actions-menu/actions-menu.css'],
-    controller: function ActionsMenuController($scope) {
+    controller: function ActionsMenuController($scope, $http, $rootScope) {
+      $scope.exportSettings = function() {
+        var exportedSettings = JSON.stringify(localStorage);
+
+        var encodedUri = "data:application/octet-stream," + encodeURIComponent(exportedSettings);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        let hoy = new Date;
+
+        var fecha = hoy.getFullYear().toString() + (hoy.getMonth() + 1).toString() + hoy.getDate().toString();
+        link.setAttribute("download", "hearthstone_pity_timer_tracker_settings_" + fecha + ".csv");
+        document.body.appendChild(link); // Required for FF
+
+        link.click(); // This will download the data file named "my_data.csv".
+
+      }
+
+
+      $scope.importSettings = function() {
+
+      }
+
+      $scope.clearSettings = function() {
+        // we should ask for confirmation before actually clearing everything
+        localStorage.clear();
+        // a more elegant solution will be used in the final version
+        alert("The settings have been reset! Page will be reloaded now");
+        location.reload();
+
+      }
+
 
     }
   });
